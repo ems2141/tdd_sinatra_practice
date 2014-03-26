0e146859b923cdba463df6bbe2b1c1b264ce74b5
Ellie Schneiders <ems2141@gmail.com>
2014-03-26 15:37:48 -0600

    user can create new task.

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ class App < Sinatra::Application
   get '/' do
     erb :index
   end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/' do
+    @task = params[:task_name]
+    erb :index
+  end
+
 end
--- spec/task_helper_spec.rb
+++ spec/task_helper_spec.rb
@@ -7,6 +7,12 @@ Capybara.app = App
 feature "Create task manager" do
   scenario "user can create a new task" do
     visit '/'
+
+    click_link "Create Task"
+    fill_in "task_name", with: "Wash Dishes"
+    click_on "Submit"
+
+    expect(page).to have_content("Wash Dishes")
   end
 end
 
